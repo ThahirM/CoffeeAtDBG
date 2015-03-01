2ef7c217240e0e27ef8e2e717c7d1fd152b65183
Thahir Maheen <thahirmaheen@icloud.com>
2015-03-01 12:24:40 +0530

    Adding SwiftDemo-Bridging-Header to bridge objc code into Swift

---
+++ SwiftDemo/SwiftDemo/SwiftDemo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SwiftDemo-Bridging-Header.h
+//  SwiftDemo
+//
+//  Created by Thahir Maheen on 01/03/15.
+//  Copyright (c) 2015 Digital Brand Group. All rights reserved.
+//
+
+#ifndef SwiftDemo_SwiftDemo_Bridging_Header_h
+#define SwiftDemo_SwiftDemo_Bridging_Header_h
+
+#import "UIImageView+WebCache.h"
+
+#endif
